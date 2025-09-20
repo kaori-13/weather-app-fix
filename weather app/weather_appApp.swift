c906720df014c7636b4c25a42e2f1c2c6fefc223
Fanny <galaxysgra@gmail.com>
2025-09-20 21:50:00 +0800

    Initial Commit

---
+++ weather app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather app
+//
+//  Created by Fanny on 2025/9/20.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
